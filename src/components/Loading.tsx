74bdd1c8791c2a027971db718d1e9afb989becef
ahsansan <maulana.ahsan@ilcs.co.id>
2023-06-07 09:39:04 +0700

    Loading merah

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,7 +4,7 @@ const Loading: FC = () => {
   
     return (
       <div className="flex justify-center items-center h-screen">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
+        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-red-900"></div>
       </div>
     );
   };
